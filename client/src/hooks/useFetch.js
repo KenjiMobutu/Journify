27e0d37435a790fa5450d8a1a0de1e26e4a1e68f
KenjiMobutu <kenjimobutu@gmail.com>
2024-09-03 00:44:31 +0200

    DEPLOYMENT: ajustment variables 19

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const useFetch = (url) => {
+const useFetch = (url, options = {}) => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
   useEffect(() => {
-    const  fetchData = async () => {
+    const fetchData = async () => {
       setLoading(true);
       console.log("Fetching data from: ", url);
       try {
-        const res = await axios.get(url);
+        const res = await axios.get(url, options);
         setData(res.data);
         console.log(res.data);
       } catch (err) {
@@ -20,12 +20,12 @@ const useFetch = (url) => {
       setLoading(false);
     };
     fetchData();
-  }, [url]);
+  }, [url, options]);
 
   const reFetch = async () => {
     setLoading(true);
     try {
-      const res = await axios.get(url);
+      const res = await axios.get(url, options);
       setData(res.data);
       console.log("refetch ");
     } catch (err) {
